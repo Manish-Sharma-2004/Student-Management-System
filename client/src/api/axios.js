d86ece6b1885bf307ddc0f475918018eb6ed0b4a
Manish-Sharma-2004 <manishsharmayalsar4042@gmail.com>
2026-06-17 09:56:36 +0530

    Added production API URL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,12 +1,27 @@
+// import axios from 'axios';
+
+// const API = axios.create({
+//   baseURL: '/api/v1',
+//   withCredentials: true,
+//   headers: { 'Content-Type': 'application/json' },
+// });
+
+// export default API;
+
+
+
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: '/api/v1',
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
   headers: { 'Content-Type': 'application/json' },
 });
 
 export default API;
+
+
+
 
 
 
